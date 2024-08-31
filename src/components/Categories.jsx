38d4b886cfd74cfc8ac1954a85e8aeae6ee58900
Tarun Jangra <tarunjangra043@gmail.com>
2024-08-31 10:10:11 +0530

    changes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-
 const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
       {categories.map((category, index) => {
+        const btnId = `filter-btn-${index + 1}`;
         return (
           <button
             type="button"
             className="filter-btn"
+            id={btnId}
             key={index}
             onClick={() => filterItems(category)}
           >
@@ -18,5 +18,3 @@ const Categories = ({ categories, filterItems }) => {
     </div>
   );
 };
-
-export default Categories;
